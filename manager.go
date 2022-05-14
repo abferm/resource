7ad85271b9015de4b1d3629a0ed54942234ac8dd
Alex Ferm <abferm@gmail.com>
2022-05-14 11:53:34 -0500

    godoc formatting improvements

--- manager.go
+++ manager.go
@@ -5,10 +5,19 @@ import (
 	"fmt"
 )
 
+// ErrManagerClosed is returned when an action is attempted on a closed manager
 var ErrManagerClosed = fmt.Errorf("manager is closed")
+
+// ErrInvalidHandle is returned when a manager is passed a handle that it isn't
+// currently managing. The handle could have been created by another manager, or
+// could already have been destroyed.
 var ErrInvalidHandle = fmt.Errorf("handle not held by this manager")
 
+// Constructor is a function provided to the Manager for creating new instances
+// of the managed resource
 type Constructor[T any] func(ctx context.Context) (T, error)
+
+// Destructor is a function provided to the Manager to destroy defective instances
 type Destructor[T any] func(T) error
 
 // Handle is a container for a managed resource
@@ -18,14 +27,16 @@ type Handle[T any] struct {
 }
 
 // Release relinquishes control of the resource back to the Manager.
+//
 // Client code MUST call Release or Destroy when done with a resource.
 func (h *Handle[T]) Release() error {
 	return h.manager.Release(h)
 }
 
 // Destroy informs the Manager that the resource is defective and must
-// 	be destroyed, the Manager is responsible for determining if it should
-// 	attempt to recreate the resource or not.
+// be destroyed, the Manager is responsible for determining if it should
+// attempt to recreate the resource or not.
+//
 // Client code MUST call Release or Destroy when done with a resource.
 func (h *Handle[T]) Destroy() error {
 	return h.manager.Destroy(h)
